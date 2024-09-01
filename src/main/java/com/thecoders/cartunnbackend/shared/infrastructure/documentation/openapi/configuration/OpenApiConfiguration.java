018365804652ef124bf585543705b3c8332e8a07
Salvador Chamorro <u201914367@upc.edu.pe>
2024-09-01 17:25:00 -0500

    feat: update jwt/OpenApi

--- src/main/java/com/thecoders/cartunnbackend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/thecoders/cartunnbackend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.thecoders.cartunnbackend.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +27,20 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("Cartunn backend Wiki Documentation")
                         .url("https://cartunn-backend.wiki.github.io/docs"));
+
+        // Add Security Scheme
+        /*final String securitySchemeName = "bearerAuth";
+        openApi.addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+        // Return OpenAPI configuration object*/
         return openApi;
     }
 }
